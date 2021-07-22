a7186f540d1014d2934968860d9411e741b889d1
juliobcpdantas <juliobcpdantas@hotmail.com>
2021-07-22 19:14:41 -0300

    Seção 2: Criando e rodando primeira aplicação com JavaScript > 7.Entendendo a execução de código JS

---
+++ execução-js/js/js.js
@@ -0,0 +1,16 @@
+//declaração de variáveis
+var nome = "Prof. Madson";
+var sobrenome = "Aguiar Rodrigues";
+
+console.log("Instrução solta em um arquivo JS");
+
+//Declaração de função
+function exibiNomeConsole (){
+
+    console.log("exibir nome: " + nome);
+
+}
+
+exibiNomeConsole();
+    
+
