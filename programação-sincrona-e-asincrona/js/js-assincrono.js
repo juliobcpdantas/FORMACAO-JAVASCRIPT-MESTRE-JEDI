06396e4d32f62dca65ef9e54c4bec5e787cce06a
juliobcpdantas <juliobcpdantas@hotmail.com>
2021-07-27 18:02:36 -0300

    Seção 3:Paradigmas de programação JavaScript, Debug, programação Síncrona e assíncrona>14-Exemplo programaçao assincrona

---
+++ programação-sincrona-e-asincrona/js/js-assincrono.js
@@ -0,0 +1,8 @@
+console.log("inicio programação síncrona.");
+
+///função timer programação assincrona
+setTimeout(function(){
+    console.log("Executando programação assincrona.");
+}, 2000);
+
+console.log("termino programação síncrona.");
