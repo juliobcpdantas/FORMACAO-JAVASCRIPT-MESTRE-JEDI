d34fe6784d456e88d1b94e2e9367332f9c8605e0
juliobcpdantas <juliobcpdantas@hotmail.com>
2021-07-27 17:10:08 -0300

    Seção 3:Paradigmas de programação JavaScript, Debug, programação Síncrona e assíncrona>10-12.exemplo programação OO e funcional JS

---
+++ Exemplo-programacao--OO-e-funciional-em-JS/js/js.js
@@ -0,0 +1,30 @@
+//funcional
+function sejaBemVindo(){
+    console.log("Seja bem vindo a Formação JavaScript Mestre Jedi!!!");
+}
+
+console.log("Chamando a função seja bem vindo sejaBemVindo():");
+sejaBemVindo();
+
+//POO - PREOGRAMAÇÃO ORIENTADA A OBJETO
+var objProfessor = {
+    nome: "Prof. Madson Aguiar",
+    curso: "Formação JavaScript Mestre Jedi!!! :)",
+    mistrarAula: function(){
+        console.log("Ministrando aula de JavaScript!");
+    }
+};
+
+console.log("objeto Professor");
+console.log(objProfessor);
+
+console.log("acessando propriedades do objeto: objProfessor.nome");
+console.log(objProfessor.nome);
+console.log(objProfessor.curso);
+
+console.log("chamando o método do objeto professor: objProfessor.mistrarAula()");
+objProfessor.mistrarAula();
+
+
+
+
